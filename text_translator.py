f944130b8f67466363057ff164dad7fe8beac1c9
itamarlevi <itamar@apiiro.com>
2024-06-30 14:22:37 +0300

    ggg

--- text_translator.py
+++ text_translator.py
@@ -1,6 +1,6 @@
 import starlette
 
-from transformers import pipeline
+#from transformers import pipeline
 
 from ray import serve
 
@@ -8,14 +8,16 @@ from ray import serve
 @serve.deployment
 class Translator:
     def __init__(self):
-        self.model = pipeline("translation_en_to_de", model="t5-small")
+        #self.model = pipeline("translation_en_to_de", model="t5-small")
 
     def translate(self, text: str) -> str:
-        return self.model(text)[0]["translation_text"]
+        #return self.model(text)[0]["translation_text"]
+        return "bbbbbbbbbbbb"
 
     async def __call__(self, req: starlette.requests.Request):
-        req = await req.json()
-        return self.translate(req["text"])
+        #req = await req.json()
+        #return self.translate(req["text"])
+        return "aaaaaaaaaaaaaaaaaa"
 
 
 app = Translator.options(route_prefix="/translate").bind()
