34288ebbc7708a2352779afb1e17431099ce0f62
itamarlevi <itamar@apiiro.com>
2024-07-03 11:37:39 +0300

    test

--- text_translator.py
+++ text_translator.py
@@ -68,7 +68,7 @@ class Translator:
         local_directory = '/tmp/phi3'
         #os.makedirs(local_directory)
         download_directory_from_s3(aws_access_key_id, aws_secret_access_key, region, bucket_name, s3_directory, local_directory)
-
+        load_model(local_directory)
 
         session = boto3.Session(
             aws_access_key_id=aws_access_key_id,
